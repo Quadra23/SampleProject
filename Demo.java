150734a299eacdd38fb581e894601bbf987c3d9e
Quadra23 <pratik.s.bhandare23041999@gmail.com>
2023-01-05 00:05:47 +0530

    changes made(Added one more function)

--- Demo.java
+++ Demo.java
@@ -2,6 +2,10 @@ class Demo{
 	public static void main(String[] args){
 		public void(){
 			System.out.println("Happy New Year");
+
+		}
+		public void(){
+			System.out.println("this year is 2023")
 		}
 	}
 }
